ac44c40c73beee7807082b38b589258ac8482b32
Juan Camilo SDS <juank0215dev@gmail.com>
2026-02-23 23:41:20 -0500

    Se crea la pantalla votación y se le agrega el sidebar.

--- src/app/features/votacion/+page.component.ts
+++ src/app/features/votacion/+page.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { SidebarComponent } from '../../shared/components/sidebar';
+import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-votacion-page',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>Votacion works</p>`,
+  imports: [CommonModule, SidebarComponent],
+  templateUrl: './+page.component.html',
+  styleUrl: './+page.component.scss',
 })
-export class VotacionPageComponent {}
+export class VotacionPageComponent {
+  totalSacerdotes = 102;
+  sacerdotesVotados = 87;
+  percentageVoted = Math.round((87 / 102) * 100);
+
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+  ) {}
+
+  onNavigate(route: string) {
+    this.router.navigate([route]);
+  }
+
+  onLogout() {
+    this.auth.clear();
+    this.router.navigate(['/auth']);
+  }
+
+  closeVotingPhase() {
+    alert('Fase de votación cerrada');
+  }
+}
