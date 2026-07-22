1708fba3b0977039414db2166c6b31266020c28e
JuanCamiloDevFrontBack <juank0215dev@gmail.com>
2026-07-22 11:23:25 -0500

    Se hacen cambios en voting-state.service.ts.

--- src/app/infrastructure/voting-state.service.ts
+++ src/app/infrastructure/voting-state.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface VotingStateResponse {
   activa: boolean;
@@ -8,41 +9,36 @@ export interface VotingStateResponse {
 
 @Injectable({ providedIn: 'root' })
 export class VotingStateService {
-
-  readonly base = 'http://localhost:3000';
-
   constructor(private http: HttpClient) {}
 
   getVotingActive(): Observable<boolean> {
-    return this.http.get<VotingStateResponse>(`${this.base}/votacion`)
-    .pipe(
-      map((response: VotingStateResponse) => response.activa)
-    );
+    return this.http
+      .get<VotingStateResponse>(`${environment.apiRestAuth}votacion`)
+      .pipe(map((response: VotingStateResponse) => response.activa));
   }
 
   updateVotingActive(data: VotingStateResponse) {
-    return this.http.put<any>(`${this.base}/votacion`, data);
+    return this.http.put<any>(`${environment.apiRestAuth}votacion`, data);
   }
 
   postVotingActive<T>(path: string, body: any) {
-    return this.http.post<T>(`${this.base}/${path}`, body);
+    return this.http.post<T>(`${environment.apiRestAuth}${path}`, body);
   }
 
   // Test
   generarEnlace(telefono: string, mensaje: string): string {
     // Eliminamos espacios, guiones o el signo '+' por si el usuario los ingresa
     const telefonoLimpio = telefono.replace(/[^0-9]/g, '');
-    
+
     // Codificamos el texto para que sea seguro en una URL (revierte espacios a %20, etc.)
     const mensajeCodificado = encodeURIComponent(mensaje);
-    
+
     return `https://wa.me/${telefonoLimpio}?text=${mensajeCodificado}`;
     //return `https://whatsapp.com/${telefonoLimpio}?text=${mensajeCodificado}`;
   }
-  
+
   sendMessageWhatsappUrl(phoneNumber: string, message: string) {
     const url = this.generarEnlace(phoneNumber, message);
     window.open(url, '_blank');
   }
-
 }
