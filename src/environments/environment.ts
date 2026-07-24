8bdfc96a95806c142e3fbc23a6fa99e4b53e64ba
JuanCamiloDevFrontBack <juank0215dev@gmail.com>
2026-07-23 23:07:09 -0500

    Se configuran los scripts, para los ambientes de desarrollo y producción.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   product: true,
-  apiRestAuth: 'http://localhost:9090/system-votaciones-sds/api/auth/',
-  apiRestSalvatorianos: 'http://localhost:9090/system-votaciones-sds/api/salvatorianos/',
+  apiRestAuth: 'http://salvatorianos/system-votaciones-sds/api/auth/',
+  apiRestSalvatorianos: 'http://salvatorianos/system-votaciones-sds/api/salvatorianos/',
   apiRestExample: 'https://spring-boot-archdiocese-management.onrender.com/archdiocese-management',
   apiRestDocker: '',
 };
